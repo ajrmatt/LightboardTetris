50958fc24f5add7a8d092c3172d229242e79582e
ajrmatt <ajrmatt@iastate.edu>
2016-11-08 13:36:25 -0600

    First commit Create Project

---
+++ app/src/main/java/edu/iastate/ajrmatt/lightboardtetris/MainMenuActivity.java
@@ -0,0 +1,13 @@
+package edu.iastate.ajrmatt.lightboardtetris;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+    }
+}
